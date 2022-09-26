78d00adbaa436f62041823da731102c3c61ab664
1sensus <90696960+1sensus@users.noreply.github.com>
2022-09-26 09:39:43 +0300

    Ref

--- Safety.laba1.cpp
+++ Safety.laba1.cpp
@@ -7,20 +7,20 @@ int main()
 	setlocale(LC_ALL, "rus");
 	ifstream fin;
 	ofstream fout;
-	fin.open("in.txt",std::ios::in);
-	fout.open("out.txt", std::ios::out);
+	fin.open("in.txt",std::ios::binary);
+	fout.open("out.txt", std::ios::binary);
 	
 	char simbol;
 	string keyword;
 	cout << "Enter a key-word"<<endl;
 	cin >> keyword;
 	size_t s_index = 0;
-	//lol
 	while (fin.is_open()&&fout.is_open())
 	{
 		
 			fin.get(simbol);
 			simbol += keyword[s_index];
+			cout << simbol<< "   " << (int)simbol<<endl;
 			fout << simbol;
 			++s_index;
 			if (s_index == keyword.size()) { s_index = 0; }
@@ -30,18 +30,19 @@ int main()
 	fin.close(); fout.close();
 	ifstream fin1;
 	ofstream fout1;
-	fin1.open("out.txt");
-	fout1.open("decode.txt");
+	fin1.open("out.txt",std::ios::binary);
+	fout1.open("decode.txt",std::ios::binary);
 	s_index = 0;
 	while (fin1.is_open()&&fout1.is_open()) 
 	{		
 			fin1.get(simbol);
-			simbol -= keyword[s_index];
-			fout1 << simbol;
+			simbol -= (char)keyword[s_index];
+			fout1 << (char)simbol;
 			++s_index;
 			if (s_index == keyword.size()) { s_index = 0; }
-			if (fin.peek() == EOF) { break; }
+			if (fin1.peek() == EOF) { break; }
 	}
-	system("pause");
 	fin1.close(); fout1.close();
+	system("pause");
+	
 }
